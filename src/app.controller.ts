4a0e153a3ae492e89d505d338cf83c0b674def1e
Allan Egidio <allan.egidio@outlook.com>
2021-12-13 22:44:43 -0300

    testing routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('/app')
 export class AppController {
 
-    @Get()
+    @Get('/test')
     test() {
-        return 'Hi There'!
+        return 'Hi There!'
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'Bye there!'
     }
 }
