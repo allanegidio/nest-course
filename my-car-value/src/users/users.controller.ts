8b0b204999c2fee703d5b29c13832a9e0bfd9a87
Allan Egidio <allan.egidio@outlook.com>
2022-01-10 07:16:12 -0300

    add user service and controller

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(
+    private readonly service: UsersService
+  ) { }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.service.find(email)
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: number) {
+    return this.service.findOne(id)
+  }
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDTO) {
+    this.service.create(body.email, body.password)
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: number) {
+    return this.service.remove(id)
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: number, @Body() user: UpdateUserDTO) {
+    return this.service.update(id, user)
+  }
+  
+}
--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>
+  ) { }
+
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password })
+    
+    return this.repository.save(user)
+  }
+
+  find(email: string) {
+    return this.repository.find({ email })
+  }
+
+  findOne(id: number) {
+    return this.repository.findOne(id)
+  }
+
+  async update(id: number, props: Partial<User>) {
+    const user = await this.repository.findOne(id)
+    
+    if(!user)
+      throw new Error('user not found')
+
+    Object.assign(user, props)
+
+    return this.repository.save(user)
+  }
+
+  async remove(id: number) {
+    const user = await this.repository.findOne(id)
+
+    if(!user)
+      throw new Error('user not found')
+    
+    return this.repository.remove(user)
+  }
+}
