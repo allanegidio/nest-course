e367deb6e18d61c6a3141ed5e6e74378b9d4ed70
Allan Egidio <allan.egidio@outlook.com>
2022-02-03 14:50:14 -0300

    fix

--- my-car-value/src/middlewares/current-user.middleware.ts
+++ my-car-value/src/middlewares/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
+import { User } from "src/users/entities/user.entity";
 import { UsersService } from "src/users/services/users.service";
+
+// Add to Request Interface
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User
+    }
+  }
+}
 
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor( 
